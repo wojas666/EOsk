560843c3b1cc1d9b33ca447950aca439b16254d1
wojas666 <wardak093@gmail.com>
2022-06-21 22:41:55 +0200

    + CreateInstructorDtoValidator with Including InstructorDto abstract validator.

---
+++ EOsk.Infrastructure/Models/Dtos/Instructors/Validators/CreateInstructorDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EOsk.Infrastructure.Models.Dtos.Instructors.Validators.Common;
+using FluentValidation;
+
+namespace EOsk.Infrastructure.Models.Dtos.Instructors.Validators
+{
+    public class CreateInstructorDtoValidator : AbstractValidator<CreateInstructorDto>
+    {
+        public CreateInstructorDtoValidator()
+        {
+            Include(new IInstructorDtoValidator());
+        }
+    }
+}
