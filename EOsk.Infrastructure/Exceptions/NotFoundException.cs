7115e809dfd9f1c65a1547a6bc4f116c1317e065
wojas666 <wardak093@gmail.com>
2022-06-21 22:37:54 +0200

    + Create custom ValidationException and NotFoundException.

---
+++ EOsk.Infrastructure/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOsk.Infrastructure.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, object key) : base($"{name} ({key} is not found.")
+        {
+            
+        }
+    }
+}
