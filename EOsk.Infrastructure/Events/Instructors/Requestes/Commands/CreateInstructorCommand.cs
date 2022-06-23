69fb87392f035f9a27dd9f64728e090b81bc761b
wojas666 <wardak093@gmail.com>
2022-06-23 15:33:12 +0200

    + Refactoring requests / dtos - added requests to keep the code in better order and to reduce possible future repetitions in the code.

---
+++ EOsk.Infrastructure/Events/Instructors/Requestes/Commands/CreateInstructorCommand.cs
@@ -0,0 +1,14 @@
+﻿using EOsk.Infrastructure.Models.Dtos.Instructors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOsk.Infrastructure.Events.Instructors.Requestes.Commands
+{
+    public class CreateInstructorCommand 
+    {
+        public CreateInstructorDto InstructorToCreated { get; set; }
+    }
+}
