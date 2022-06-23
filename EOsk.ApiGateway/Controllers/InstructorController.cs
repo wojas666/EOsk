e8b44f6d8d62aa5f4dd0453b48da36f4fcb67de1
wojas666 <wardak093@gmail.com>
2022-06-23 15:29:34 +0200

    + Implementing GetInstructorById and Get

--- EOsk.ApiGateway/Controllers/InstructorController.cs
+++ EOsk.ApiGateway/Controllers/InstructorController.cs
@@ -1,4 +1,6 @@
-﻿using EOsk.Infrastructure.Models.Dtos.Instructors;
+﻿using EOsk.Infrastructure.Events.Instructors.Requestes.Commands;
+using EOsk.Infrastructure.Events.Instructors.Requestes.Queries;
+using EOsk.Infrastructure.Models.Dtos.Instructors;
 using EOsk.Infrastructure.Models.Dtos.Instructors.Validators;
 using EOsk.Infrastructure.Responses;
 using FluentValidation;
@@ -12,10 +14,34 @@ namespace EOsk.ApiGateway.Controllers
     public class InstructorController : Controller
     {
         private readonly IBusControl _bus;
+        private readonly IConfiguration _configuration;
+        private readonly IRequestClient<GetInstructorByIdRequest> _getInstructorByIdRequest;
+        private readonly IRequestClient<GetInstructorListRequest> _getInstructorListRequest;
 
-        public InstructorController(IBusControl busControl)
+        public InstructorController(IBusControl busControl, IConfiguration configuration,
+            IRequestClient<GetInstructorByIdRequest> getInstructorByIdRequest,
+            IRequestClient<GetInstructorListRequest> getInstructorListRequest)
         {
             _bus = busControl;
+            _getInstructorByIdRequest = getInstructorByIdRequest;
+            _getInstructorListRequest = getInstructorListRequest;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetInstructorById(Guid id)
+        {
+            var getInstructorByIdRequest = new GetInstructorByIdRequest() { Id = id };
+            var instructor = await _getInstructorByIdRequest.GetResponse<InstructorDto>(getInstructorByIdRequest);
+
+            return Accepted(instructor);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var instructors = await _getInstructorListRequest.GetResponse<InstructorDto[]>(new GetInstructorListRequest());
+
+            return Accepted(instructors);
         }
 
         [HttpPost]
@@ -34,9 +60,11 @@ namespace EOsk.ApiGateway.Controllers
                 return BadRequest(createInstructorResponse);
             }
 
+            CreateInstructorCommand command = new CreateInstructorCommand() { InstructorToCreated = newInstructor };
+
             var uri = new Uri("rabbitmq://localhost/create_instructor");
             var endpoint = await _bus.GetSendEndpoint(uri);
-            await endpoint.Send(newInstructor);
+            await endpoint.Send(command);
 
             createInstructorResponse.Message = "Instruktor utworzony pomyślnie!";
 
